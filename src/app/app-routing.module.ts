919e544566bcc6e650536ffa03e8a98fd0f1626c
Duy Chinh <hduychinh@gmail.com>
2020-08-06 15:41:24 +0700

    Add basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodoListComponent },
+  { path: 'detail', component: TodoDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
